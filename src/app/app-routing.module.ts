c16f9674bce2890ba94c47cf2952b0d68548921f
Sapphire <zivanovic.markko002@gmail.com>
2024-02-01 20:57:43 +0100

    Domaci 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { OffersComponent } from './offers/offers.component';
+import { ReccomendationsComponent } from './reccomendations/reccomendations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'offers', component: OffersComponent},
+  { path: 'recommendations', component: ReccomendationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,14 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  item_list = ['Offer', 'Reccomentations', 'About us'];
-  selected_option: string;
-
-  constructor() {
-    this.selected_option = this.item_list[0];
-  }
-
-  selectOption(option: string): void {
-    this.selected_option = option;
-  }
 }
